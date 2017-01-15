f0c6f9df0b36ee77463562f8a126a7e6f7387965
Acemad <ouessai.aes@gmail.com>
2017-01-15 01:37:01 +0100

    Add support for nested files/folders.
    Past version would only act on the first level of folders/files on the device, and would not operate on subfolders and nested files. This should fix the problem. Used https://www.reddit.com/user/dam_teenagers script instead.

--- Unhider.bat
+++ Unhider.bat
@@ -1,10 +1,7 @@
 @echo off
 CHCP 65001
 set /p drive=Please input the Vita drive letter (with :) (ex F:):
-pushd "%drive:~,2%"
-for /f "delims=" %%i in ('dir "%drive:~,2%" /b /a') do attrib -h -s "%%i"
+echo Please wait...
+attrib -h "%drive:~,2%"\* /S /D -s -r
 echo Done !
 pause
-
-
-REM AES-11/02/2014
